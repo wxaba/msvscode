9bb7b30b2cd44428e35e9e4dcc07a24f54ec3706
ouken <wxaba@outlook.com>
2023-11-06 14:48:44 +0800

    footer

--- node-learn/01创建http服务.js
+++ node-learn/01创建http服务.js
@@ -4,26 +4,22 @@
  const path = require('path')
  //02 创建服务
 let rootpath = '/Users/wuzei/Documents/msvscode/work_pxpt'
-
  const server = http.createServer((request, response) => {
-   
    let {pathname} = new URL(request.url, 'http://127.0.0.1');
    console.log(path.parse(rootpath+request.url));
-
    fs.readFile(rootpath+pathname,(err,data)=>{
       if(err){
          console.log(err);
          response.writeHead(200,{"Content-type":"text/html;charset=utf-8"});
-         response.end('<h1 style="font-size:280px;">页面还没做哦 🐷</h1>');
+          var data2= fs.readFileSync('/Users/wuzei/Documents/msvscode/work_pxpt/NotFound.html')
+         response.end(data2);
          return
       }
       else{
          response.write(data);
          response.end(); //设置响应体，结束响应
       }      
-      
    })
-
  });
  //03 监听端口，启动服务
  server.listen(80, () => {
