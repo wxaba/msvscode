667c008f1ca96e6157b3dbdc715ff4888f21ebc6
ouken <wxaba@outlook.com>
2023-11-06 14:13:47 +0800

    server test

--- node-learn/01创建http服务.js
+++ node-learn/01创建http服务.js
@@ -1,9 +1,29 @@
  //01 导入 http 模块
  const http = require('http')
+ const fs = require('fs')
+ const path = require('path')
  //02 创建服务
+let rootpath = '/Users/wuzei/Documents/msvscode/work_pxpt'
+
  const server = http.createServer((request, response) => {
-    response.setHeader('content-type','text/html;sharset=utf-8');
-    response.end('Hello HTTP Server! 吧'); //设置响应体，结束响应
+   
+   let {pathname} = new URL(request.url, 'http://127.0.0.1');
+   console.log(path.parse(rootpath+request.url));
+
+   fs.readFile(rootpath+pathname,(err,data)=>{
+      if(err){
+         console.log(err);
+         response.writeHead(200,{"Content-type":"text/html;charset=utf-8"});
+         response.end('<h1 style="font-size:280px;">页面还没做哦 🐷</h1>');
+         return
+      }
+      else{
+         response.write(data);
+         response.end(); //设置响应体，结束响应
+      }      
+      
+   })
+
  });
  //03 监听端口，启动服务
  server.listen(80, () => {
