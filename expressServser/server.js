c9c9f121efb9cd77ae34bbed40232ea5e2c1409b
ouken <wxaba@outlook.com>
2023-11-14 11:00:10 +0800

    rewrite homepage

--- expressServser/server.js
+++ expressServser/server.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const path =require("path");
 const app = express();
-
+app.use('../work_pxpt', express.static('public'));
 app.get('/',function(req, res){
-    res.send('hello Express haha!');
-}).listen(8888,() => {
+res.sendFile('/Users/wuzei/Documents/msvscode/work_pxpt/index.html')
+})
+app.get('/*',function(req, res){
+    res.sendFile('/Users/wuzei/Documents/msvscode/work_pxpt'+req.url)
+    })
+app.listen(80,() => {
     console.log('服务已启动')
 });
+
