d0a3c0fa340410a454105d11c0bad4ecd175f602
ouken <wxaba@outlook.com>
2023-11-15 10:22:38 +0800

    优化

--- expressServser/server.js
+++ expressServser/server.js
@@ -6,4 +6,8 @@ res.sendFile('/Users/wuzei/Documents/msvscode/work_pxpt/index.html')
 app.get('/*',function(req, res){
     res.sendFile('/Users/wuzei/Documents/msvscode/work_pxpt'+req.url)
     })
+app.post('/login',function(req, res){
+    console.log(req.headers)
+    res.send('登陆成功')
+})
 app.listen(80,() => {console.log('服务已启动')});
