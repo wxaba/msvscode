1b6cd536d9e5532f3a9830c3a9a30c347c88ad2d
ouken <wxaba@outlook.com>
2023-11-16 10:54:13 +0800

    get post data

--- expressServser/server.js
+++ expressServser/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
 app.get('/',function(req, res){
 res.sendFile('/Users/wuzei/Documents/msvscode/web/index.html')
 })
@@ -12,8 +16,7 @@ app.post('/login',function(req, res){
 })
 
 app.post('/register', function(req,res){
-    console.log(req.query)
+    console.log(req.body.realname)
     res.send('注册')
 })
-
 app.listen(80,() => {console.log('服务已启动')});
