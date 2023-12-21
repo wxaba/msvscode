a14322729107854c9e758bcfeaa40b1c87fc5b43
ouken <wxaba@outlook.com>
2023-12-21 12:49:51 +0800

    e

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const path = require('path');
 const app = express();
+
+const webPath = path.join(path.dirname(__dirname),'web');
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 app.get('/',function(req, res){
-res.sendFile('../web/pages/index.html')
+    res.sendFile(path.join(webPath,'pages','index.html'));
+    console.log(webPath);
 })
 app.get('/*',function(req, res){
-    res.sendFile('web/pages'+req.url)
+    console.log(req.url)
+    console.log(req.path)
+    res.sendFile(path.join(webPath,req.path))
     })
 app.post('/login',function(req, res){
     console.log(req.headers)
@@ -18,5 +24,9 @@ app.post('/login',function(req, res){
 app.post('/register', function(req,res){
     console.log(req.body)
     res.send('注册')
+})
+
+app.all('*',(req, res)=>{
+    res.sendFile(path.join(webPath,'pages','NotFound.html'))
 })
 app.listen(80,() => {console.log('服务已启动')});
