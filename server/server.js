1564638fbe81c6b64b845bda6da569b34d5f9678
ouken <wxaba@outlook.com>
2023-12-22 09:28:48 +0800

    login

--- server/server.js
+++ server/server.js
@@ -2,30 +2,18 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-const rootPath = path.join(path.dirname(__dirname),'web');
-
-app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(path.dirname(__dirname),'web')));
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-app.get('/',function(req, res){
-    res.sendFile(path.join(rootPath,'index.html'));
-})
-app.get('/*',function(req, res){
-    console.log(req.url)
-    console.log(req.path)
-    res.sendFile(path.join(rootPath,req.path))
-    })
-app.post('/login',function(req, res){
-    console.log(req.headers)
-    res.send('登陆成功')
-})
+app.get('/login',(req, res)=>{
+    res.sendFile(path.join(path.dirname(__dirname),'web','login.html'))
+});
 
-app.post('/register', function(req,res){
-    console.log(req.body)
-    res.send('注册')
-})
+app.post('/login',(req, res)=>{
+    res.send(req.body);
+    console.log(typeof req.body);
+    console.log(req.body);
+});
 
-app.all('*',(req, res)=>{
-    res.sendFile(path.join(webPath,'pages','NotFound.html'))
-})
 app.listen(80,() => {console.log('服务已启动')});
